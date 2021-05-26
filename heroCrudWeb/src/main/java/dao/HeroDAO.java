09f627a504f6a542f56827fec7b7a0481847ae84
Denis <denisdmm@gmail.com>
2021-05-26 12:04:01 -0300

    heroCrudWeb inicial

---
+++ heroCrudWeb/src/main/java/dao/HeroDAO.java
@@ -0,0 +1,16 @@
+
+import java.sql.Connection;
+
+public class HeroDAO {
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        String url = "jdbc:postgresql:localhost:5432/hero";
+        String user = "hero";
+        String password = "hero";
+        
+    }
+    
+}
